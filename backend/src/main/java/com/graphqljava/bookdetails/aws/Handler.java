8f7cf3dfaac74782b229cacf41f4d508fe9585e1
Blake Bordovsky <blakebordovsky@gmail.com>
2022-05-05 12:50:55 -0500

    Add info log

--- backend/src/main/java/com/graphqljava/bookdetails/aws/Handler.java
+++ backend/src/main/java/com/graphqljava/bookdetails/aws/Handler.java
@@ -6,6 +6,7 @@ import com.coxautodev.graphql.tools.SchemaParser;
 import com.coxautodev.graphql.tools.SchemaParserBuilder;
 import com.graphqljava.bookdetails.resolvers.MutationResolver;
 import com.graphqljava.bookdetails.resolvers.QueryResolver;
+import com.sun.media.jfxmedia.logging.Logger;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
@@ -17,6 +18,8 @@ public class Handler implements RequestHandler<InputType, String> {
   @Override
   public String handleRequest(InputType input, Context context) {
     init();
+
+    Logger.logMsg(Logger.INFO, input.toString());
 
     ExecutionInput exec = ExecutionInput.newExecutionInput()
       .query(input.getQuery())
